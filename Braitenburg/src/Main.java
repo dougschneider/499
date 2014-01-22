e9925f6fb5294e0d4a184c26a616411093745a62
Douglas Schneider <ds3@cs.ualberta.ca>
2014-01-22 14:46:39 -0700

    added the rest of the braitenburg bot states

--- Braitenburg/src/Main.java
+++ Braitenburg/src/Main.java
@@ -30,5 +30,56 @@ public class Main {
 		}
 		
 	}
+	
+	public static void aggressive(LightSensor rightSensor, LightSensor leftSensor)
+	{
+		int maxTime = 2000;
+		int currentTime = 0;
+		int delta = 10;
+		while(currentTime < maxTime)
+		{
+			currentTime += delta;
+			int rightPower = leftSensor.getLightValue();
+			int leftPower = rightSensor.getLightValue();
+			MotorPort.C.controlMotor(leftPower, MotorPort.FORWARD);
+			MotorPort.A.controlMotor(rightPower, MotorPort.FORWARD);
+			Delay.msDelay(delta);
+		}
+		
+	}
+	
+	public static void love(LightSensor rightSensor, LightSensor leftSensor)
+	{
+		int maxTime = 2000;
+		int currentTime = 0;
+		int delta = 10;
+		while(currentTime < maxTime)
+		{
+			currentTime += delta;
+			int rightPower = 100 - rightSensor.getLightValue();
+			int leftPower = 100 - leftSensor.getLightValue();
+			MotorPort.C.controlMotor(leftPower, MotorPort.FORWARD);
+			MotorPort.A.controlMotor(rightPower, MotorPort.FORWARD);
+			Delay.msDelay(delta);
+		}
+		
+	}
+	
+	public static void explore(LightSensor rightSensor, LightSensor leftSensor)
+	{
+		int maxTime = 2000;
+		int currentTime = 0;
+		int delta = 10;
+		while(currentTime < maxTime)
+		{
+			currentTime += delta;
+			int rightPower = 100 - leftSensor.getLightValue();
+			int leftPower = 100 - rightSensor.getLightValue();
+			MotorPort.C.controlMotor(leftPower, MotorPort.FORWARD);
+			MotorPort.A.controlMotor(rightPower, MotorPort.FORWARD);
+			Delay.msDelay(delta);
+		}
+		
+	}
 
 }
