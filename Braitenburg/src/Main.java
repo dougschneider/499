aec169c04ba89c49a3d89878b0c128839214102f
Douglas Schneider <ds3@cs.ualberta.ca>
2014-01-23 13:00:32 -0700

    Completed Braitenberg.

--- Braitenburg/src/Main.java
+++ Braitenburg/src/Main.java
@@ -5,25 +5,30 @@ import lejos.util.Delay;
 
 
 public class Main {
+	
+	private static int scalingFactor = 30; 
 
 	public static void main(String[] args){
 		LightSensor rightSensor = new LightSensor(SensorPort.S1);
 		LightSensor leftSensor = new LightSensor(SensorPort.S4);
 		rightSensor.setFloodlight(false);
 		leftSensor.setFloodlight(false);
-		coward(rightSensor, leftSensor);
+//		coward(rightSensor, leftSensor);
+//		aggressive(rightSensor, leftSensor);
+//		love(rightSensor, leftSensor);
+		explore(rightSensor, leftSensor);
 	}
 	
 	public static void coward(LightSensor rightSensor, LightSensor leftSensor)
 	{
-		int maxTime = 2000;
+		int maxTime = 5000;
 		int currentTime = 0;
 		int delta = 10;
 		while(currentTime < maxTime)
 		{
 			currentTime += delta;
-			int rightPower = rightSensor.getLightValue();
-			int leftPower = leftSensor.getLightValue();
+			int rightPower = rightSensor.getLightValue() + scalingFactor;
+			int leftPower = leftSensor.getLightValue() + scalingFactor;
 			MotorPort.C.controlMotor(leftPower, MotorPort.FORWARD);
 			MotorPort.A.controlMotor(rightPower, MotorPort.FORWARD);
 			Delay.msDelay(delta);
@@ -33,14 +38,14 @@ public class Main {
 	
 	public static void aggressive(LightSensor rightSensor, LightSensor leftSensor)
 	{
-		int maxTime = 2000;
+		int maxTime = 5000;
 		int currentTime = 0;
 		int delta = 10;
 		while(currentTime < maxTime)
 		{
 			currentTime += delta;
-			int rightPower = leftSensor.getLightValue();
-			int leftPower = rightSensor.getLightValue();
+			int rightPower = leftSensor.getLightValue() + scalingFactor;
+			int leftPower = rightSensor.getLightValue() + scalingFactor;
 			MotorPort.C.controlMotor(leftPower, MotorPort.FORWARD);
 			MotorPort.A.controlMotor(rightPower, MotorPort.FORWARD);
 			Delay.msDelay(delta);
@@ -50,16 +55,17 @@ public class Main {
 	
 	public static void love(LightSensor rightSensor, LightSensor leftSensor)
 	{
-		int maxTime = 2000;
+		int maxTime = 5000;
 		int currentTime = 0;
 		int delta = 10;
 		while(currentTime < maxTime)
 		{
 			currentTime += delta;
-			int rightPower = 100 - rightSensor.getLightValue();
-			int leftPower = 100 - leftSensor.getLightValue();
+			int rightPower = Math.min(100, 100 + scalingFactor - rightSensor.getLightValue());
+			int leftPower = Math.min(100, 100 + scalingFactor - leftSensor.getLightValue());
 			MotorPort.C.controlMotor(leftPower, MotorPort.FORWARD);
 			MotorPort.A.controlMotor(rightPower, MotorPort.FORWARD);
+			System.out.println(rightPower);
 			Delay.msDelay(delta);
 		}
 		
@@ -67,14 +73,14 @@ public class Main {
 	
 	public static void explore(LightSensor rightSensor, LightSensor leftSensor)
 	{
-		int maxTime = 2000;
+		int maxTime = 5000;
 		int currentTime = 0;
 		int delta = 10;
 		while(currentTime < maxTime)
 		{
 			currentTime += delta;
-			int rightPower = 100 - leftSensor.getLightValue();
-			int leftPower = 100 - rightSensor.getLightValue();
+			int rightPower = Math.min(100, 100 + scalingFactor - leftSensor.getLightValue());
+			int leftPower = Math.min(100, 100 + scalingFactor - rightSensor.getLightValue());
 			MotorPort.C.controlMotor(leftPower, MotorPort.FORWARD);
 			MotorPort.A.controlMotor(rightPower, MotorPort.FORWARD);
 			Delay.msDelay(delta);
