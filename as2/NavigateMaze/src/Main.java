361f8bc0fcff18993a7607e4311110aabc1af6ee
Douglas Schneider <ds3@cs.ualberta.ca>
2014-02-12 14:43:27 -0700

    added Naive Bayes to p3

--- as2/NavigateMaze/src/Main.java
+++ as2/NavigateMaze/src/Main.java
@@ -5,6 +5,7 @@ import lejos.nxt.MotorPort;
 import lejos.nxt.SensorPort;
 import lejos.nxt.TouchSensor;
 import lejos.nxt.UltrasonicSensor;
+import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.functions.MultilayerPerceptron;
 import weka.core.Instance;
 import weka.core.Instances;
@@ -22,15 +23,16 @@ public class Main {
 			loader.setSource(new File("../../logp3.csv"));
 			data = loader.getDataSet();
 		} catch (Exception e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		
 		data.setClassIndex(data.numAttributes()-1);
-		
-		MultilayerPerceptron mlp = new MultilayerPerceptron();
+
+//		MultilayerPerceptron mlp = new MultilayerPerceptron();
+		NaiveBayes nb = new NaiveBayes();
 		try {
-			mlp.buildClassifier(data);
+//			mlp.buildClassifier(data);
+			nb.buildClassifier(data);
 //			Evaluation eval = new Evaluation(data);
 //			eval.crossValidateModel(mlp, data, 10, new Random(1));
 //			System.out.println(eval.pctCorrect());
@@ -53,22 +55,16 @@ public class Main {
 			i.setValue(2, rightTouch.isPressed() ? "TRUE" : "FALSE");
 			i.setValue(3, leftTouch.isPressed() ? "TRUE" : "FALSE");
 			try {
-				double instanceClass = mlp.classifyInstance(i);
-				System.out.println(instanceClass);
+//				double instanceClass = mlp.classifyInstance(i);
+				double instanceClass = nb.classifyInstance(i);
 				if (instanceClass == 2.0) {// FORWARD
-//					leftMotor.controlMotor(30, MotorPort.FORWARD);
-//					rightMotor.controlMotor(20, MotorPort.FORWARD);
 					leftMotor.controlMotor(25, MotorPort.FORWARD);
 					rightMotor.controlMotor(20, MotorPort.FORWARD);
 				}
 				else if (instanceClass == 1.0) {//BACKRIGHT
-//					leftMotor.controlMotor(30, MotorPort.BACKWARD);
-//					rightMotor.controlMotor(100, MotorPort.BACKWARD);
 					leftMotor.controlMotor(30, MotorPort.BACKWARD);
 					rightMotor.controlMotor(70, MotorPort.BACKWARD);
 				} else if(instanceClass == 0.0) {//BACKLEFT
-//					leftMotor.controlMotor(100, MotorPort.BACKWARD);
-//					rightMotor.controlMotor(30, MotorPort.BACKWARD);
 					leftMotor.controlMotor(70, MotorPort.BACKWARD);
 					rightMotor.controlMotor(30, MotorPort.BACKWARD);
 				}
