abd1cd8ed718498a475f696cd66939ef93a56176
Aaron Krebs <akrebs@ualberta.ca>
2014-02-23 21:46:12 -0700

    Test script changes.

--- proj1/test.py
+++ proj1/test.py
@@ -1,5 +1,8 @@
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageEnhance
 
 image = Image.open('18.jpg')
-image = image.filter(ImageFilter.EDGE_ENHANCE_MORE)
+image = image.convert('L')
+#editor = ImageEnhance.Contrast(image)
+#image = editor.enhance(1.3)
+#image = image.enhance(ImageFilter.EDGE_ENHANCE_MORE)
 image.save('output18.jpg')
