f86ca672c47faa36f5c3c6bfc62041eff3b35e6d
Douglas Schneider <ds3@ualberta.ca>
2014-03-03 12:50:26 -0700

    make the arff creation handle n csv's

--- proj1/createARFF.py
+++ proj1/createARFF.py
@@ -1,36 +1,41 @@
 import sys
 
+if len(sys.argv) < 3:
+    raise Exception("must provide -n argument");
+numFiles = int(sys.argv[2])
+
 test = False
-if len(sys.argv) > 1:
-    test = bool(sys.argv[1] == '--test')
+if len(sys.argv) > 3:
+    test = bool(sys.argv[3] == '--test')
+
+imgs = dict()
+
+def handleCSVs(classID, className):
+    for i in range(1, numFiles+1):
+        with open("%s-%d.csv" % (classID, i), "r") as f:
+            first = True
+            for line in f:
+                if first:
+                    first = False
+                    continue
+                attrs = line.rstrip("\n").split(", ")
+                if i == 1:
+                    imgs[attrs[1]] = ""
+                for j in range(2, len(attrs)):
+                    imgs[attrs[1]] += attrs[j] + ","
+                if i == numFiles:
+                    if test:
+                        imgs[attrs[1]] += "?\n"
+                    else:
+                        imgs[attrs[1]] += ("%s\n" % className)
+
 
 preamble = "@RELATION plate\n\n"
-first = True
 with open("out.arff", "w+") as out:
-    imgs = dict()
-    with open("4-1.csv", "r") as f:
-        for line in f:
-            if first:
-                first = False
-                continue
-            attrs = line.rstrip("\n").split(", ")
-            imgs[attrs[1]] = ""
-            for i in range(2, len(attrs)):
-                imgs[attrs[1]] += attrs[i] + ","
 
-    first = True
-    with open("4-2.csv", "r") as f:
-        for line in f:
-            if first:
-                first = False
-                continue
-            attrs = line.rstrip("\n").split(", ")
-            for i in range(2, len(attrs)):
-                imgs[attrs[1]] += attrs[i] + ","
-            if test:
-                imgs[attrs[1]] += "?\n"
-            else:
-                imgs[attrs[1]] += "FOUR\n"
+    handleCSVs("4", "FOUR")
+    handleCSVs("5", "FIVE")
+    handleCSVs("o", "OTHER")
 
     attrs = imgs[imgs.keys()[0]].split(",")
     for i in range(len(attrs)-1):
@@ -38,56 +43,6 @@ with open("out.arff", "w+") as out:
     preamble += "@ATTRIBUTE class {FOUR, FIVE, OTHER}\n"
     preamble += "\n@DATA\n"
     out.write(preamble)
-
-    first = True
-    with open("5-1.csv", "r") as f:
-        for line in f:
-            if first:
-                first = False
-                continue
-            attrs = line.rstrip("\n").split(", ")
-            imgs[attrs[1]] = ""
-            for i in range(2, len(attrs)):
-                imgs[attrs[1]] += attrs[i] + ","
-
-    first = True
-    with open("5-2.csv", "r") as f:
-        for line in f:
-            if first:
-                first = False
-                continue
-            attrs = line.rstrip("\n").split(", ")
-            for i in range(2, len(attrs)):
-                imgs[attrs[1]] += attrs[i] + ","
-            if test:
-                imgs[attrs[1]] += "?\n"
-            else:
-                imgs[attrs[1]] += "FIVE\n"
-
-    first = True
-    with open("o-1.csv", "r") as f:
-        for line in f:
-            if first:
-                first = False
-                continue
-            attrs = line.rstrip("\n").split(", ")
-            imgs[attrs[1]] = ""
-            for i in range(2, len(attrs)):
-                imgs[attrs[1]] += attrs[i] + ","
-
-    first = True
-    with open("o-2.csv", "r") as f:
-        for line in f:
-            if first:
-                first = False
-                continue
-            attrs = line.rstrip("\n").split(", ")
-            for i in range(2, len(attrs)):
-                imgs[attrs[1]] += attrs[i] + ","
-            if test:
-                imgs[attrs[1]] += "?\n"
-            else:
-                imgs[attrs[1]] += "OTHER\n"
 
     for key, val in imgs.items():
         out.write(val)
