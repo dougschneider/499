4795c6a7afc765e91c7f5d146ca8933afea9fbc4
Aaron Krebs <akrebs@ualberta.ca>
2014-03-15 13:00:08 -0600

    Store data works.

--- as3/NavigateTerrain/src/NavigateTerrain.java
+++ as3/NavigateTerrain/src/NavigateTerrain.java
@@ -1,30 +1,35 @@
 import java.io.IOException;
+import java.util.ArrayList;
 
+import lejos.nxt.LightSensor;
 import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
 import lejos.robotics.navigation.DifferentialPilot;
 
 public class NavigateTerrain {
 
 	public static int NUM_SWEEPS = 1;
-	
+
 	public static int SPEED = 20;
 	public static int STEP_TIME = 20;
 	public static int PIVOT_TIME = 1000;
-	
+
 	public static DifferentialPilot pilot;
 
 	public static TrackerReader tracker;
 	public static Tuple<Double, Double> topRight;
 	public static Tuple<Double, Double> bottomLeft;
 
+	public static ArrayList<Triple<Double, Double, Integer>> data = new ArrayList<Triple<Double, Double, Integer>>();
+
 	public static void main(String[] args) {
 		tracker = new TrackerReader();
 		tracker.start();
-		
+
 		pilot = new DifferentialPilot(56, 120, Motor.C, Motor.A);
 		pilot.setTravelSpeed(SPEED);
 		pilot.setRotateSpeed(SPEED);
-		
+
 		// set up the tracker
 		pauseAndPrompt("Select the robot's marker to track it");
 
@@ -32,16 +37,16 @@ public class NavigateTerrain {
 		topRight = getCorner(tracker, "top right");
 		System.out.println("Top Right: " + topRight.x + ", " + topRight.y);
 		bottomLeft = getCorner(tracker, "bottom left");
-		System.out.println("Bottom Left: " + bottomLeft.x + ", " + bottomLeft.y);
-		
+		System.out
+				.println("Bottom Left: " + bottomLeft.x + ", " + bottomLeft.y);
+
 		// gather data
 		doGatherData(pilot, tracker, topRight, bottomLeft);
-
 	}
 
-	public static void doGatherData(
-			DifferentialPilot pilot, TrackerReader tracker,
-			Tuple<Double, Double> topRight, Tuple<Double, Double> bottomLeft) {
+	public static void doGatherData(DifferentialPilot pilot,
+			TrackerReader tracker, Tuple<Double, Double> topRight,
+			Tuple<Double, Double> bottomLeft) {
 		int numSweeps = 0;
 		while (numSweeps < NUM_SWEEPS) {
 			doSingleSweep(pilot, tracker, topRight, bottomLeft);
@@ -49,18 +54,18 @@ public class NavigateTerrain {
 		}
 	}
 
-	private static void doSingleSweep(
-			DifferentialPilot pilot, TrackerReader tracker,
-			Tuple<Double, Double> topRight, Tuple<Double, Double> bottomLeft) {
+	private static void doSingleSweep(DifferentialPilot pilot,
+			TrackerReader tracker, Tuple<Double, Double> topRight,
+			Tuple<Double, Double> bottomLeft) {
 		Boolean sweepComplete = false;
-		
+
 		// tell user to place robot
 		pauseAndPrompt("Place robot in top right corner, aimed left");
 
 		while (sweepComplete == false) {
-			doPass(pilot, Direction.LEFT, topRight, bottomLeft);
+			doPass(tracker, pilot, Direction.LEFT, topRight, bottomLeft);
 			pivot(pilot, Direction.LEFT);
-			doPass(pilot, Direction.RIGHT, topRight, bottomLeft);
+			doPass(tracker, pilot, Direction.RIGHT, topRight, bottomLeft);
 			pivot(pilot, Direction.RIGHT);
 			sweepComplete = isSweepComplete(tracker, topRight, bottomLeft);
 		}
@@ -75,21 +80,27 @@ public class NavigateTerrain {
 		return false;
 	}
 
-	private static void doPass(
-			DifferentialPilot pilot, Direction direction,
+	private static void doPass(TrackerReader tracker, DifferentialPilot pilot, Direction direction,
 			Tuple<Double, Double> topRight, Tuple<Double, Double> bottomLeft) {
-		System.out.println("Robot: " + tracker.x + ", " + tracker.y + "\tCorner: " + bottomLeft.x + ", " + bottomLeft.y);
+		System.out.println("Robot: " + tracker.x + ", " + tracker.y
+				+ "\tCorner: " + bottomLeft.x + ", " + bottomLeft.y);
+		LightSensor lightsensor = new LightSensor(SensorPort.S1);
 		if (direction == Direction.RIGHT) {
 			while (tracker.x < topRight.x) {
-				driveForward(pilot, STEP_TIME);
+				doPassActions(tracker, pilot, lightsensor);
 			}
 		} else {
 			while (tracker.x > bottomLeft.x) {
-				driveForward(pilot, STEP_TIME);
+				doPassActions(tracker, pilot, lightsensor);
 			}
 		}
 		System.out.println("Done Pass");
-
+	}
+	
+	private static void doPassActions(TrackerReader tracker, DifferentialPilot pilot, LightSensor lightsensor) {
+		driveForward(pilot, STEP_TIME);
+		data.add(new Triple<Double, Double, Integer>(tracker.x,
+				tracker.y, lightsensor.getLightValue()));
 	}
 
 	private static void driveForward(DifferentialPilot pilot, int time) {
