220771f55244724d3622facbc171f40f77ddfd4c
Aaron Krebs <akrebs@ualberta.ca>
2014-03-17 14:03:27 -0600

    Finished up reward function, started training agent for it.
    Works really well.

--- as3/TravelStraight/src/RoboEnvironment.java
+++ as3/TravelStraight/src/RoboEnvironment.java
@@ -41,19 +41,60 @@ public class RoboEnvironment extends AbstractEnvironmentSingle {
 	public IState defaultInitialState() {
 		return new SensorState(this, SensorController.getFrontDist(), SensorController.getBackDist(), SensorController.getLightValue());
 	}
+	
+	private static int getDiff(int distance, int middle) {
+		if (distance > middle) {
+			return middle - distance;
+		} else {
+			return distance - middle;
+		}
+	}
 
 	@Override
 	public double getReward(IState s1, IState s2, IAction a) {
-		int bucket = s2.hashCode();
 		int reward = -1000;
+		int oldbucket = s1.hashCode();
+		int newbucket = s2.hashCode();
+		int olddiff = getDiff(oldbucket, CENTER_CM);
+		int newdiff = getDiff(newbucket, CENTER_CM);
 		
-		if (bucket > CENTER_CM) {
-			reward = CENTER_CM - bucket;
-		} else {
-			reward = bucket - CENTER_CM;
+		boolean action_right = ((RoboAction)a).direction == RoboAction.RIGHT;
+		boolean action_left = ((RoboAction)a).direction == RoboAction.LEFT;
+		boolean action_straight = ((RoboAction)a).direction == RoboAction.STRAIGHT;
+		
+		// less than since they're negative values
+		boolean away_from_center = newdiff < olddiff;
+		
+		boolean left_of_center = newbucket < CENTER_CM;
+		boolean right_of_center = newbucket > CENTER_CM;
+		boolean center = newbucket == CENTER_CM;
+		
+		// moved between buckets
+		if (oldbucket != newbucket) {
+			if (away_from_center) {
+				reward = newdiff * 2;
+			} else {
+				reward = newdiff;
+			}
+		}
+		// stayed in same bucket
+		else if (oldbucket == newbucket) {
+			if (center) {
+				if (action_straight) {
+					reward = newdiff + 10;
+				} else {
+					reward = newdiff;
+				}
+			} else if ((left_of_center && action_left) || (right_of_center && action_right)) {
+				reward = newdiff * 3;
+			} else 	if ((left_of_center && action_straight) || (right_of_center && action_straight)) {
+				reward = newdiff * 2;
+			} else if ((left_of_center && action_right) || (right_of_center && action_left)) {
+				reward = newdiff;
+			}
 		}
 		
-		System.out.println("\tBucket: "	+ bucket + "\tReward: " + reward + "\tAction: " + a.toString());
+		System.out.println("Old Bucket: " + oldbucket + "\tNew Bucket: " + newbucket + "\tReward: " + reward + "\tAction: " + a.toString());
 		return reward;
 	}
 
