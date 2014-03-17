7dd741da59756e3ad0674fb187513409fcac4434
Douglas Schneider <ds3@ualberta.ca>
2014-03-17 13:34:28 -0600

    got a good map for part2

--- as3/ProduceMap/src/Main.java
+++ as3/ProduceMap/src/Main.java
@@ -35,7 +35,7 @@ public class Main {
 		SelfOrganizingMap som = null;
 		ObjectInputStream in;
 		try {
-			in = new ObjectInputStream(new FileInputStream("../mapcluster-before.model"));
+			in = new ObjectInputStream(new FileInputStream("../p2good.model"));
 			som = (SelfOrganizingMap) in.readObject();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -51,8 +51,9 @@ public class Main {
 		double avgElevation = 0;
 		try {
 			double[][][] stats = som.getStatistics();
-			for(int i = 0; i < som.numberOfClusters(); ++i)
-				avgElevation += stats[2][i][2];
+			//for(int i = 0; i < som.numberOfClusters(); ++i)
+            // attribute/cluster/row
+			System.out.println(avgElevation += stats[0][0][2]);
 			avgElevation/=som.numberOfClusters();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -61,55 +62,31 @@ public class Main {
 		
 		BufferedImage image = new BufferedImage(600, 400, BufferedImage.TYPE_INT_RGB);
 		
-		BufferedWriter bw = null;
-		try {
-			bw = new BufferedWriter(new FileWriter(new File("../tdata.arff")));
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		int[] colours = {0x0000FF, 0x00FF00, 0xFF0000, 0xFFFFFF};
-		for(int x = 125; x <= 500; x += 5)
+		int[] colours = {0x0000FF, 0x00FF00, 0xFF0000, 0xFFFFFF, 0x00FFFF, 0xFF00FF, 0xFFFF00, 0xDDDDDD, 0xAAAAAA};
+		for(int x = 125; x <= 500; x += (500-125)/20)
 		{
-			for(int y = 80; y <= 345; y += 5)
+			for(int y = 80; y <= 345; y += (345-80)/20)
 			{
 				Instance instance = data.firstInstance();
 				instance.setValue(0, x);
 				instance.setValue(1, y);
 				instance.setValue(2, avgElevation);
 				try {
-					bw.write(x + "," + y + "," + "?" + "\n");
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				try {
-					image.setRGB(x, y, colours[som.clusterInstance(instance)]);
+                    int cluster = getClosestCluster(x, y, som);
+                    System.out.print(cluster);
+                    for(int i = -2; i <= 2; ++i)
+                    {
+                        for(int j = -2; j <= 2; ++j)
+                        {
+                            image.setRGB(x+i, y+j, colours[cluster]);
+                        }
+                    }
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-		}
-		for(int i = 0; i < data.numInstances(); ++i)
-		{
-			Instance instance = data.instance(i);
-			int x = (int) instance.value(0);
-			int y = (int) instance.value(1);
-			try {
-				image.setRGB(x, y, colours[som.clusterInstance(instance)]);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		try {
-			bw.close();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+            System.out.println();
 		}
 		
 		File out = new File("../map.png");
@@ -121,4 +98,32 @@ public class Main {
 		}
 	}
 
+    public static int getClosestCluster(int x, int y, SelfOrganizingMap som)
+    {
+        int numClusters = 0;
+        double[][][] stats = null;
+        try{
+            numClusters = som.numberOfClusters();
+            stats = som.getStatistics();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        double closest = -1;
+        int cluster = -1;
+        for(int i = 0; i < numClusters; ++i)
+        {
+            double meanx = stats[0][i][2];
+            double meany = stats[1][i][2];
+            double dist = Math.sqrt(Math.pow(x-meanx, 2) + Math.pow(y-meany, 2));
+            if(closest == -1 || dist < closest)
+            {
+                closest = dist;
+                cluster = i;
+            }
+        }
+
+        return cluster;
+    }
 }
