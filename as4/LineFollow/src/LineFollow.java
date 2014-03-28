c3fec046765611defcc43c2f3b9b40bf24b26b1e
Aaron Krebs <akrebs@ualberta.ca>
2014-03-27 19:29:52 -0600

    Refactored public interface to remove arguments that should always be 0.

--- as4/LineFollow/src/LineFollow.java
+++ as4/LineFollow/src/LineFollow.java
@@ -17,9 +17,9 @@ public class LineFollow {
 		MotorPort leftMotor = MotorPort.C;
 		MotorPort rightMotor = MotorPort.A;
 		Controllers.bangBang(sensor, leftMotor, rightMotor);
-		Controllers.P(3, 0, 0, sensor, leftMotor, rightMotor);
-		Controllers.PD(1.8, 0, 1.5, sensor, leftMotor, rightMotor);
-		Controllers.PI(2, 0.1, 0, sensor, leftMotor, rightMotor);
+		Controllers.P(3, sensor, leftMotor, rightMotor);
+		Controllers.PD(1.8, 1.5, sensor, leftMotor, rightMotor);
+		Controllers.PI(2, 0.1, sensor, leftMotor, rightMotor);
 		Controllers.PID(1.8, 0.1, 0.3, sensor, leftMotor, rightMotor);
 	}
 }
