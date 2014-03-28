424f024547cbe641a54db1abde9766959c18b9fd
Aaron Krebs <akrebs@ualberta.ca>
2014-03-27 19:28:15 -0600

    Refactored base controllers class to use single implementation.

--- as4/ControllersBase/src/controllers/Controllers.java
+++ as4/ControllersBase/src/controllers/Controllers.java
@@ -36,15 +36,6 @@ public class Controllers {
 		System.exit(0);
 	}
 
-	/**
-	 * P controller implementation
-	 * @param Kp
-	 * @param Ki
-	 * @param Kd
-	 * @param sensor
-	 * @param leftMotor
-	 * @param rightMotor
-	 */
 	public static void P(double Kp, double Ki, double Kd, LightSensor sensor, MotorPort leftMotor,
 			MotorPort rightMotor) {
 //		double Kp = 3;
@@ -52,42 +43,7 @@ public class Controllers {
 //		double Kd = 0;
 
 		int targetValue = 35;
-		int error = 0;
-		int lastError = 0;
-
-		int basePower = 10;
-
-		double integral = 0;
-		double derivative = 0;
-
-		while (true) {
-			int current = sensor.getLightValue();
-			error = current - targetValue;
-			System.out.println(current);
-
-			integral = integral + error;
-			derivative = error - lastError;
-
-			int turn = (int) Math.round(Kp * error + Ki * integral + Kd
-					* derivative);
-			// turn /= 200;
-
-			leftMotor.controlMotor(basePower + turn, MotorPort.FORWARD);
-			rightMotor.controlMotor(basePower - turn, MotorPort.FORWARD);
-			lastError = error;
-			// 28/45
-			if (Button.RIGHT.isPressed())
-				break;
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		leftMotor.controlMotor(100, MotorPort.STOP);
-		rightMotor.controlMotor(100, MotorPort.STOP);
-		System.exit(0);
+		runPID(targetValue, Kp, Ki, Kd, sensor, leftMotor, rightMotor);
 	}
 
 	public static void PD(double Kp, double Ki, double Kd, LightSensor sensor, MotorPort leftMotor,
@@ -97,42 +53,7 @@ public class Controllers {
 //		double Kd = 1.5;
 
 		int targetValue = 40;
-		int error = 0;
-		int lastError = 0;
-
-		int basePower = 10;
-
-		double integral = 0;
-		double derivative = 0;
-
-		while (true) {
-			int current = sensor.getLightValue();
-			error = current - targetValue;
-			System.out.println(current);
-
-			integral = integral + error;
-			derivative = error - lastError;
-
-			int turn = (int) Math.round(Kp * error + Ki * integral + Kd
-					* derivative);
-			// turn /= 200;
-
-			leftMotor.controlMotor(basePower + turn, MotorPort.FORWARD);
-			rightMotor.controlMotor(basePower - turn, MotorPort.FORWARD);
-			lastError = error;
-			// 28/45
-			if (Button.RIGHT.isPressed())
-				break;
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		leftMotor.controlMotor(100, MotorPort.STOP);
-		rightMotor.controlMotor(100, MotorPort.STOP);
-		System.exit(0);
+		runPID(targetValue, Kp, Ki, Kd, sensor, leftMotor, rightMotor);
 	}
 
 	public static void PI(double Kp, double Ki, double Kd, LightSensor sensor, MotorPort leftMotor,
@@ -142,42 +63,7 @@ public class Controllers {
 //		double Kd = 0;
 
 		int targetValue = 35;
-		int error = 0;
-		int lastError = 0;
-
-		int basePower = 10;
-
-		double integral = 0;
-		double derivative = 0;
-
-		while (true) {
-			int current = sensor.getLightValue();
-			error = current - targetValue;
-			System.out.println(current);
-
-			integral = integral + error;
-			derivative = error - lastError;
-
-			int turn = (int) Math.round(Kp * error + Ki * integral + Kd
-					* derivative);
-			// turn /= 200;
-
-			leftMotor.controlMotor(basePower + turn, MotorPort.FORWARD);
-			rightMotor.controlMotor(basePower - turn, MotorPort.FORWARD);
-			lastError = error;
-			// 28/45
-			if (Button.RIGHT.isPressed())
-				break;
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		leftMotor.controlMotor(100, MotorPort.STOP);
-		rightMotor.controlMotor(100, MotorPort.STOP);
-		System.exit(0);
+		runPID(targetValue, Kp, Ki, Kd, sensor, leftMotor, rightMotor);
 	}
 
 	public static void PID(double Kp, double Ki, double Kd, LightSensor sensor, MotorPort leftMotor,
@@ -187,42 +73,7 @@ public class Controllers {
 //		double Kd = 0.3;
 
 		int targetValue = 36;
-		int error = 0;
-		int lastError = 0;
-
-		int basePower = 10;
-
-		double integral = 0;
-		double derivative = 0;
-
-		while (true) {
-			int current = sensor.getLightValue();
-			error = current - targetValue;
-			System.out.println(current);
-
-			integral = integral + error;
-			derivative = error - lastError;
-
-			int turn = (int) Math.round(Kp * error + Ki * integral + Kd
-					* derivative);
-			// turn /= 200;
-
-			leftMotor.controlMotor(basePower + turn, MotorPort.FORWARD);
-			rightMotor.controlMotor(basePower - turn, MotorPort.FORWARD);
-			lastError = error;
-			// 28/45
-			if (Button.RIGHT.isPressed())
-				break;
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		leftMotor.controlMotor(100, MotorPort.STOP);
-		rightMotor.controlMotor(100, MotorPort.STOP);
-		System.exit(0);
+		runPID(targetValue, Kp, Ki, Kd, sensor, leftMotor, rightMotor);
 	}
 	
 	private static void runPID(int targetValue, double Kp, double Ki, double Kd, LightSensor sensor, MotorPort leftMotor,
@@ -256,7 +107,6 @@ public class Controllers {
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
