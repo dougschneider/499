5ca92287f500fa775effa5a4890645631ee3ea2c
Aaron Krebs <akrebs@ualberta.ca>
2014-04-09 17:59:10 -0600

    Cleanup and comments.

--- proj2/Race/src/main/RobotInteractionMembers.java
+++ proj2/Race/src/main/RobotInteractionMembers.java
@@ -5,17 +5,29 @@ import lejos.nxt.MotorPort;
 import lejos.nxt.addon.OpticalDistanceSensor;
 import lejos.nxt.remote.RemoteMotor;
 
+/**
+ * Storage class for Motors, Sensors, etc.
+ * Provides some utility methods for common actions, but it intended to
+ * be a dump object, not for controlling the robot.
+ */
 public class RobotInteractionMembers {
 
+	// sensors
 	public LightSensor lightSensor;
+	public OpticalDistanceSensor leftObstacleSensor;
+	public OpticalDistanceSensor rightObstacleSensor;
+	public OpticalDistanceSensor farRightObstacleSensor;
+	
+	/* Motors
+	 * 
+	 * DifferentialPilot needs RemoteMotor instances, so each motor is
+	 * here twice.
+	 */
 	public MotorPort rightMotor;
 	public MotorPort leftMotor;
 	public RemoteMotor rightRemoteMotor;
 	public RemoteMotor leftRemoteMotor;
-	public OpticalDistanceSensor leftObstacleSensor;
-	public OpticalDistanceSensor rightObstacleSensor;
-	public OpticalDistanceSensor farRightObstacleSensor;
-
+	
 	public static final double WHEEL_DIAMETER = 56;
 	public static final double TRACK_WIDTH = 120;
 
@@ -40,11 +52,17 @@ public class RobotInteractionMembers {
 		this.leftObstacleSensor = leftObstacleSensor;
 	}
 
+	/**
+	 * Stop the motors.
+	 */
 	public void stop() {
 		rightMotor.controlMotor(100, MotorPort.STOP);
 		leftMotor.controlMotor(100, MotorPort.STOP);
 	}
-	
+
+	/**
+	 * Set the motors to move forward at slow speed.
+	 */
 	public void forward() {
 		leftMotor.controlMotor(30, MotorPort.FORWARD);
 		rightMotor.controlMotor(30, MotorPort.FORWARD);
