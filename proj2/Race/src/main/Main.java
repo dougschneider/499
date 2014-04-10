060cd751a74edcd72407957199f1e59abe27e908
Aaron Krebs <akrebs@ualberta.ca>
2014-04-09 18:09:12 -0600

    Cleanup and comments.

--- proj2/Race/src/main/Main.java
+++ proj2/Race/src/main/Main.java
@@ -6,10 +6,14 @@ import lejos.nxt.MotorPort;
 import lejos.nxt.SensorPort;
 import lejos.nxt.addon.OpticalDistanceSensor;
 
+/**
+ * Implementation of Project 2.
+ */
 public class Main {
 
 	public static void main(String[] args) {
 
+		// set up sensors
 		LightSensor lightSensor = new LightSensor(SensorPort.S1);
 		OpticalDistanceSensor rightObstacleSensor = new OpticalDistanceSensor(
 				SensorPort.S3);
@@ -18,10 +22,12 @@ public class Main {
 		OpticalDistanceSensor leftObstacleSensor = new OpticalDistanceSensor(
 				SensorPort.S4);
 
+		// put all the sensors in container class
 		RobotInteractionMembers ioMembers = new RobotInteractionMembers(
 				lightSensor, MotorPort.A, MotorPort.C, Motor.A,
 				Motor.C, rightObstacleSensor, leftObstacleSensor, farRightObstacleSensor);
 
+		// init, configure, and run racer
 		Racer racer = new Racer(ioMembers);
 		racer.configure();
 		try {
