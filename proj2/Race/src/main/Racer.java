6a161b9bcc3817d07c5e9eeced1cc15c6dab4c17
Aaron Krebs <akrebs@ualberta.ca>
2014-04-09 18:07:38 -0600

    Cleanup and comments.

--- proj2/Race/src/main/Racer.java
+++ proj2/Race/src/main/Racer.java
@@ -8,32 +8,36 @@ import arbitrators.SingleThreadArbitrator;
 import behaviours.ObstacleAvoidBehaviour;
 import behaviours.PIDBehaviour;
 
+/**
+ * Top-Level implementation of a behaviour-driven robot. Goes around the
+ * circuit and avoids obstacles.
+ * Internal implementation is a subsumption architecture that uses a PID
+ * controller to follow the outer edge of the circuit and custom moves
+ * to avoid obstacles using optical sensors.
+ */
 public class Racer {
-	// private int NORMAL_ZONE_READING = 45;
-	// private int SPECIAL_ZONE_READING = 40;
-	// private int INNER_EDGE_READING = 30;
-	// private int OUTER_EDGE_READING = 24;
 
+	// PID controller target values
 	private int NORMAL_ZONE = 52;
 	private int SPECIAL_ZONE = 45;
-//	private int NORMAL_TARGET = 39;
-//	private int SPECIAL_TARGET = 35;
 	private int NORMAL_TARGET = 35;
 	private int SPECIAL_TARGET = 35;
-
-//	private int EXTREME_TURN_INTERVENTION_TIME = 400;
 	
-	private static final int NUM_SENSOR_POLLS = 100;
-
+	// container for motors and sensors so they don't all have to be passed around
 	private RobotInteractionMembers ioMembers = null;
-
-	private boolean isConfigured = false;
+	// arbitrator for subsumption architecture
 	private SingleThreadArbitrator arbitrator = null;
+	
+	private boolean isConfigured = false;
 
 	public Racer(RobotInteractionMembers ioMembers) {
 		this.ioMembers = ioMembers;
 	}
 
+	/**
+	 * Configure the racer. Gets any required initial sensor readings and
+	 * sets up the behaviours. Try to make sure the robot is stationary.
+	 */
 	public void configure() {
 		Behavior[] behaviors = null;
 		
@@ -46,20 +50,25 @@ public class Racer {
 		obstacleAvoider.configure(NORMAL_ZONE, SPECIAL_ZONE, NORMAL_TARGET,
 				SPECIAL_TARGET);
 
-//		TimedRecoveryBehavior extremeTurnRecovery = new TimedRecoveryBehavior(
-//				EXTREME_TURN_INTERVENTION_TIME, ioMembers);
-//		extremeTurnRecovery.configure(NORMAL_ZONE, SPECIAL_ZONE);
-
-		// Highest priority behavior with highest index.
+		// Highest priority behavior has highest index in array.
 		behaviors = new Behavior[] { pidDriver,
 				obstacleAvoider };
-		this.arbitrator = new SingleThreadArbitrator(behaviors, true);
 		
-		ioMembers.stop();
+		// returnWhenInactive == true so that we can tell when no behaviour kicks in
+		this.arbitrator = new SingleThreadArbitrator(behaviors, true);
 		
 		this.isConfigured = true;
 	}
 
+	/**
+	 * Start the racer's behaviour.
+	 * 
+	 * @throws ConfigurationException
+	 * 		When configure() has not been called.
+	 * @throws RuntimeException
+	 * 		When the arbitrator exits. This means no behaviour has taken control
+	 * 		and probably means you have the behaviours in an incorrect priority order.
+	 */
 	public void race() throws ConfigurationException, RuntimeException {
 		if (!this.isConfigured) {
 			throw new ConfigurationException("Run configure() before race()");
